d39bbf4da5fe61f7adb789c20468361d4302b300
Justin Chase <justin.m.chase@gmail.com>
2018-08-09 21:12:39 -0500

    Add sdb for sentence detection

--- specs/nutshell_spec.js
+++ specs/nutshell_spec.js
@@ -1,20 +1,21 @@
 var in_a = require('../lib/in-a-nutshell');
 var fs = require('fs');
 
-var text = fs.readFileSync("specs/test_data/ants.txt").toString();
+var ants = fs.readFileSync("specs/test_data/ants.txt").toString();
+var tesla = fs.readFileSync('specs/test_data/tesla.txt').toString();
 
 describe('in-a-nutshell', function() {
-    it('should summarize a given text', function() {
+    it('should summarize a given ants', function() {
         expect(
-            in_a.nutshell(text, 1)
+            in_a.nutshell(ants, 1)
         ).toEqual(
-            "This common black ant eats other insects, and also aphid honeydew."
+            'This common black ant eats other insects, and also aphid honeydew.'
         );
     });
 
     it('should return as much sentences as declared', function() {
         expect(
-            in_a.sentenceTokenize(in_a.nutshell(text, 2)).length
+            in_a.sentenceTokenize(in_a.nutshell(ants, 2)).length
         ).toEqual(
             2
         );
@@ -22,9 +23,17 @@ describe('in-a-nutshell', function() {
 
     it('should return the summary in the declared output type', function() {
         expect(
-            in_a.nutshell(text, 1, "html")
+            in_a.nutshell(ants, 1, 'html')
         ).toEqual(
-            "<p>This common black ant eats other insects, and also aphid honeydew.</p>"
+            '<p>This common black ant eats other insects, and also aphid honeydew.</p>'
         );
     });
+
+    it('should handle complex sentences', function() {
+        expect(
+            in_a.nutshell(tesla, 1)
+        ).toEqual(
+            '“That’s a clear factual statement,” said John C. Coffee Jr., a professor at Columbia Law School who specializes in corporate law and securities fraud.'
+        )
+    })
 });
